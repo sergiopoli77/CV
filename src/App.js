4b81a538961baf3f0a467f5c4c460ed6842728b3
sergiopoli77 <S22210048@student.unklab.ac.id>
2024-11-19 06:50:42 +0800

    add aboutme component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import Header from "./Components/Header";
+import About from "./Components/AboutMe";
 
 const App = () => {
   return (
@@ -14,7 +15,9 @@ const App = () => {
 
       <div className="container">
         <Header />
-        <main className="right-side"></main>
+        <main className="right-side">
+          <About />
+        </main>
       </div>
     </div>
   );
